6eb25c11c7d5eca096509c289dea6cf34164fda7
dumblole <move2234@gmail.com>
2018-05-04 23:15:52 -0400

    Added project python files
    run main.py
    striangle.py is the drawing function

---
+++ main.py
@@ -0,0 +1,85 @@
+'''
+Sierpinski's Triangle by Max Chiu
+Foundations of Computer Science
+Mr. Gordon | Block A
+
+This is a recursive program that draws Sierpinski's triangle based off of
+a couple of factors:
+How many degrees does user want to see (iterations)
+Does user want to see the drawing process
+How long should one of the sides of the base triangle be
+Color of the triangle
+'''
+
+import turtle
+import math
+import sys
+import striangle
+
+
+def err_mess():
+    '''
+    Ends the program when an invalid input is entered
+    '''
+
+    print('Make sure you input values in the correct format! \nClose out the program and run it again.')
+    sys.exit("Please input values in the correct format! \nClose out the program and run it again.")
+
+
+#Only runs if main module / It's not imported
+if __name__ == "__main__":
+    '''
+    Initializes the turtle, queries the user for multiple
+    inputs for the characteristics of the triangle. Positions the turtle
+    so that the triangle will be centered.
+    '''
+
+    color_options = ['nofill','fill','rgbfill']
+    
+    #Program Description. Same one as the description of the project located in the project docstring. Not the function docstring.
+    print("This is a recursive program that draws Sierpinski's triangle based off of \na couple of factors: \nHow many degrees does user want to see (iterations) \nDoes user want to see the drawing process \nHow long should one of the sides of the base triangle be \nColor of the triangle\nWhen the program is done drawing the triangle, just click in the window to exit\n")
+    
+    #Fail safe for incorrect user inputs
+    try: 
+        deg = int(input("How many degrees (iterations) of Sierpinski's Triangle would you like to see? \nPlease enter an integer. \n"))
+        proc = input("Would you like to see the triangle being drawn live? \nPlease enter either 'y' or 'n' \n").lower()
+        length = int(input("How long (pixels) would you like to have one of the sides of the base triangle be? \nOtherwise, enter '0' for standard length(500 px) \nPlease enter an integer \n"))
+        color = input("Would you like the triangle to have no fill, fill (black), or RGB fill? \nInput is 'nofill', 'fill', 'rgbfill', respectively. \n")
+    except:
+        print('Make sure you input values in the correct format! \nClose out the program and run it again.')
+
+
+    #configuring the turtle based off of user input
+        
+
+    if length == 0:
+        length = 500
+    if proc == 'n':
+        striangle.pen.speed(0)
+        striangle.wn.tracer(0,0)
+        striangle.wn.delay(0)
+    elif proc != 'y':
+        err_mess()
+        
+    #I'm doing this to check if the user input was valid, also because rgb fill changes color after every triangle so it will be sent into the triangle function 
+    if color in color_options:
+        deg-=1
+        #MOVE turtle pen to the left by half of the length and down by length/4 * sqrt3
+        #This basically centers the triangle
+
+        print(striangle.pen.xcor(),striangle.pen.ycor())
+        striangle.pen.up()
+        print(striangle.pen.xcor(),striangle.pen.ycor())
+        striangle.pen.goto(striangle.pen.xcor()-length/2,striangle.pen.ycor()-length/4*3**(1/2))
+        print(striangle.pen.xcor(),striangle.pen.ycor())
+        striangle.pen.down()
+        striangle.triangle(deg,length,color)
+        striangle.pen.up()
+        striangle.pen.goto(-200,-length/4*3**(1/2)*1.25)
+        striangle.pen.down()
+        striangle.pen.write("Sierpinski's Triangle By: Max Chiu | Degree #"+str(deg+1), font=("Arial", 16, "bold"))
+        striangle.wn.exitonclick()
+    else:
+        err_mess()
+    
+
---
+++ striangle.py
@@ -0,0 +1,77 @@
+'''
+Sierpinski's Triangle by Max Chiu
+Foundations of Computer Science
+Mr. Gordon | Block A
+
+This is the function that actually draws Sierpinski's Triangle.
+Imported to main.py
+This can run by itself but it is recommended to run main.py for a better user experience
+NOTE: Does not center the triangle if program was run from here
+'''
+
+
+
+import turtle
+import random
+
+
+#Declaring variables
+wn = turtle.Screen()
+pen = turtle.Turtle()
+#default color mode is 1
+wn.colormode(255)
+
+
+def triangle(degree,length,color):
+    '''
+    This is the function that actually draws Sierpinski's Triangle
+    
+    degree is the amt of iterations
+    degree set is just the integer of iterations the user wants to see.
+    
+    length is the base length of the triangle when first called
+    length set is the integer length in pixels for the length of one of the sides of the base equillateral equation
+    
+    color is whatever the user chooses to fill the triangle
+    color can be 'nofill', 'fill', or 'rgbfill'
+    '''
+        #Color config
+    if color == 'nofill':
+        pen.fillcolor(255,255,255)
+    if color == 'fill':
+        pen.fillcolor(0,0,0)
+    if color == 'rgbfill':
+        pen.fillcolor(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
+    #Base case. If iterations reach zero, then just stop the program
+    if degree <= 0 :
+        pen.begin_fill()
+        for _ in range(3):
+            pen.forward(length)
+            #We have to turn 120 to make the 60 degree angle for the equillateral triangle
+            pen.left(120)
+        pen.end_fill()
+    else:
+        '''
+        Calls itself three times, one for each of the three triangles as seen in degree 1
+        And for every next degree there will be three more triangles in each of the three triangles and three more for the three triangles in each of the three original triangles and so on.
+        Basically, if the function it was calling not on itself but on a different function
+        that only drew a triangle, it would be able to draw one degree and reset to the
+        bottom left of the triangle which is where the pen started
+        Making it recursive makes it able to draw multiple degrees
+        It doesn't bother with drawing each degree at a time but instead draws the degree as requested but instead positions the pen every time it calls on itself
+        by iterating the degree value to 0 while dividing the length by 2 everytime and moving the pen
+        '''
+        triangle(degree-1,length/2,color)
+        pen.left(60)
+        pen.forward(length/2)
+        pen.right(60)
+        triangle(degree-1,length/2,color)
+        pen.left(60)
+        pen.backward(length/2)
+        pen.right(60)
+        pen.forward(length/2)
+        triangle(degree-1,length/2,color)
+        pen.backward(length/2)
+        #Referring back to the comment made eariler, this resets the pen to the bottom left of the triangle
+
